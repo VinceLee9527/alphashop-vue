64da06b5581af670276a77c35b9df69284d35684
VinceLee9527 <vlee9527@gmail.com>
2021-11-18 03:19:55 +0800

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Checkout from '../views/Checkout.vue'
+import NotFound from '../views/NotFound.vue'
+
 
 Vue.use(VueRouter)
 
@@ -33,6 +35,11 @@ const routes = [
       },
     ]
     },
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound
+    }
 ]
 
 const router = new VueRouter({
